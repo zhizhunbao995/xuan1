97535d340bc75f3520cd78fe8da29cf07a84ad7c
xuan <zhizhunbao995@163.com>
2013-05-09 16:03:16 +0800

    Signed-off-by: xuan <zhizhunbao995@163.com>

--- modules/tshop-um-full-screen/tshop-um-full-screen.php
+++ modules/tshop-um-full-screen/tshop-um-full-screen.php
@@ -31,167 +31,7 @@
 
 $bgcolor=$_MODULE['cs-bgcolor'];
 
-$boxHeight=$_MODULE['img_height']?$_MODULE['img_height']:380;
-
-$allshop = $_MODULE['cetagorylist'];//获得类目
-
-$itemNum=0; //读取用户提供的宝贝数量
-
-$itemList=array();  //宝贝数据
-
-$imgList=array();   //图片数据
-
-$provide_imgList=array(
-
-'assets/images/slide/slide5_03.jpg',
-
-'assets/images/slide/slide5_03.jpg',
-
-'assets/images/slide/slide5_03.jpg',
-
-'assets/images/slide/slide5_06.jpg',
-
-'assets/images/slide/slide5_06.jpg',
-
-'assets/images/slide/slide5_06.jpg',
-
-'assets/images/slide/slide5_06.jpg',
-
-'assets/images/slide/slide5_06.jpg',
-
-'assets/images/slide/slide5_06.jpg'
-
-);
-
-//从用户那个获取数据
-
-$itemString=$_MODULE['cs1-item'];
-
-$idList=explode(',',$itemString);
-
-//如果用户没有选择宝贝,计算数组数量还是为1,所以要处理下数据
-
-$idList=clearnull($idList);
-
-
-
-if(count($idList)==0){
-
-	//宝贝选择器没有数据
-
-	$itemList=getItems(9,$shopCategoryManager,$itemManager);//获得宝贝对象列表
-
-	//从系统获取的数据,一定要判断个数
-
-	$itemNum=count($itemList);
-
-}else{
-
-	//宝贝选择器有数据
-
-	$itemList=$itemManager->queryByIds($idList,"hotsell");  
-
-	$itemNum=count($idList);
-
-}
-
-//print_r($itemList);
-
-//获取图片
-
-$bimg=$_MODULE['cs1-bimg'];  //获取大图片
-
-$bimgArr=explode(',',$bimg);
-
-$bimgArr=clearnull($bimgArr);
-
-if(count($bimgArr)==0){
-
-$imgList=$provide_imgList;
-
-}else{
-
-$imgList=$bimgArr;
-
-}
-
-//function
-
-function clearnull($items){
-
-	foreach($items as $item){
-
-		if($item){
-
-		$newItems[]=$item;
-
-		}else{
-
-		continue;
-
-		};
-
-	}
-
-	return $newItems;
-
-}
-
-function getItems($itemNum,$shopCategoryManager,$itemManager){
-
-	$catArr=array();//分类数组
-
-	$allShopCategory=$shopCategoryManager->queryAll();
-
-	
-
-	foreach($allShopCategory as $shopCategory){
-
-		//echo $shopCategory->id."\n";
-
-		$catArr[]=$shopCategory->id;      //类目ID
-
-	}
-
-	//$sonShopCategory = $shopCategoryManager->querySubCategories($catArr[1]);子类目选择
-
-	$itemArr=array();//产品数组
-
-	foreach($catArr as $catId){
-
-		$itemList=$itemManager->queryByCategory($catId,"hotsell",$itemNum);//查询宝贝列表
-
-		foreach($itemList as $item){
-
-		//echo $catId;
-
-			//判断获取的产品是否为空
-
-			if($item->exist){
-
-				array_push($itemArr,$item);
-
-				if(count($itemArr) > ($itemNum-1)) 
-
-					break;
-
-			}
-
-		}
-
-			if(count($itemArr) > ($itemNum-1)) break;
-
-		}
-
-		//echo var_dump($itemArr);
-
-		//echo count($itemArr);
-
-		return $itemArr;    
-
-	}
-
-// 全新图片地址
+$boxHeight=$_MODULE['img_height']?$_MODULE['img_height']:560;
 
 $num = 5;//图片地址和跳转地址一共为10
 ?>
@@ -233,7 +73,7 @@ data-widget-config="{'navCls':'ks-switchable-nav','contentCls':'ks-switchable-co
 	for($i = 1;$i<=$num;$i++){
 
 ?>
-				<li class="big-piclist">
+				<li class="big-piclist" style="background:<?php echo $_MODULE["img".$i."_color"]=="" ? "#fbdade": $_MODULE["img".$i."_color"];?>">
 					<a href="<? echo $_MODULE["img".$i."_href"];?>
 						" target="_blank">
 						<img  src = "<? echo $_MODULE["img".$i];?>" alt=""></a>
@@ -247,17 +87,17 @@ data-widget-config="{'navCls':'ks-switchable-nav','contentCls':'ks-switchable-co
 			<ul class="ks-switchable-nav">
 				<?php
 
-	for($i = 1;$i<=$num;$i++){
+				for($i = 1;$i<=$num;$i++){
 
-	if($i == 1){
+				if($i == 1){
 
-		$classon = "ks-active";
+					$classon = "ks-active";
 
-	}else{
+				}else{
 
-		$classon = "";
+					$classon = "";
 
-	}
+				}
 
 ?>
 		<li class="<? echo $classon;?>">
